bac6f494519cef959a61eab5f450eb41a93e5c5a
Katrina Fuller <trinafuller@att.net>
2019-09-10 14:32:38 -0600

    connected search to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home container">
     <div class="row">
+      <div class="col-12">
+        <search />
+      </div>
+    </div>
+    <div class="row">
       <ApodDetails />
     </div>
   </div>
@@ -9,11 +14,13 @@
 <script>
 // @ is an alias to /src
 import ApodDetails from "../components/ApodDetails";
+import Search from "../components/Search";
 
 export default {
   name: "home",
   components: {
-    ApodDetails
+    ApodDetails,
+    Search
   }
 };
 </script>
