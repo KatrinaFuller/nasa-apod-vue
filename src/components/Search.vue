2a3de8a7859893f8a5d3d08d01998f845aa778d1
Katrina Fuller <trinafuller@att.net>
2019-09-10 14:25:04 -0600

    created search

---
+++ src/components/Search.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="search col-12">
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <button
+          class="btn btn-outline-warning"
+          type="button"
+          id="button-addon1"
+          @click="getApod()"
+        >Search</button>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Enter a date YYYY-MM-DD"
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+        v-model="query"
+      />
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "search",
+  data() {
+    return {
+      query: ""
+    };
+  },
+  computed: {
+    getApod() {
+      this.$store.dispatch("getApod", this.query);
+    }
+  },
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
