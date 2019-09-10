b3b151a180edd89f36339d46eef1e2837eec00f2
Katrina Fuller <trinafuller@att.net>
2019-09-10 15:11:46 -0600

    search working and able to change picutres

--- src/store.js
+++ src/store.js
@@ -5,8 +5,10 @@ import axios from 'axios'
 Vue.use(Vuex)
 
 let api = axios.create({
-  baseURL: 'https://api.nasa.gov/planetary/apod?api_key=HDd1PrJDOJfI5fSYkAOGyxTdIe8J0K1s7ePyf8bJ'
+  baseURL: 'https://api.nasa.gov/planetary/'
 })
+
+let urlParams = "apod?api_key=HDd1PrJDOJfI5fSYkAOGyxTdIe8J0K1s7ePyf8bJ&date="
 
 export default new Vuex.Store({
   state: {
@@ -20,7 +22,7 @@ export default new Vuex.Store({
   actions: {
     async getApod({ commit, dispatch }, query) {
       try {
-        let res = await api.get(query)
+        let res = await api.get(urlParams + query)
         commit('setApod', res.data)
       } catch (error) {
         console.error(error)
