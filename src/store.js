f547f3214572d98beedf4fde5a1e5897146fd0b9
Katrina Fuller <trinafuller@att.net>
2019-09-10 13:57:34 -0600

    axios linked

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+let api = axios.create({
+  baseURL: 'https://api.nasa.gov/planetary/apod?api_key=HDd1PrJDOJfI5fSYkAOGyxTdIe8J0K1s7ePyf8bJ'
+})
+
 export default new Vuex.Store({
   state: {
-
+    pictures: {}
   },
   mutations: {
 
